9aea0a7aa305555a4f1ddb5f6e1f99919c27d44c
moto <mnxamoto@mail.ru>
2018-08-24 18:38:47 +0300

    Проект загружен

---
+++ ShifrovanieOpenKey/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace ShifrovanieOpenKey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.MaximumSize = new System.Drawing.Size(133, 113);
+            this.dataGridView1.MinimumSize = new System.Drawing.Size(133, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(133, 113);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.Frozen = true;
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 30;
+            // 
+            // Column2
+            // 
+            this.Column2.Frozen = true;
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(3, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(291, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.tableLayoutPanel1.SetRowSpan(this.textBox1, 4);
+            this.textBox1.Size = new System.Drawing.Size(473, 382);
+            this.textBox1.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 138F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.listBox2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 118F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(767, 388);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(3, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(216, 3);
+            this.listBox2.Name = "listBox2";
+            this.tableLayoutPanel1.SetRowSpan(this.listBox2, 4);
+            this.listBox2.Size = new System.Drawing.Size(69, 382);
+            this.listBox2.TabIndex = 5;
+            this.listBox2.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox2_DrawItem);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(3, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(141, 3);
+            this.listBox1.Name = "listBox1";
+            this.tableLayoutPanel1.SetRowSpan(this.listBox1, 4);
+            this.listBox1.Size = new System.Drawing.Size(69, 382);
+            this.listBox1.TabIndex = 4;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 388);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(767, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(117, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 410);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ShifrovanieOpenKey/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShifrovanieOpenKey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int p, q, fi, n, d, eps, S;
+        int[] C, M, M2, H;
+        string text;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(5);
+            dataGridView1.Rows[0].Cells[0].Value = "p";
+            dataGridView1.Rows[1].Cells[0].Value = "q";
+            dataGridView1.Rows[2].Cells[0].Value = "M[]";
+            dataGridView1.Rows[3].Cells[0].Value = "H[0]";
+            dataGridView1.Rows[4].Cells[0].Value = "M[]";
+
+            dataGridView1.Rows[0].Cells[1].Value = 17;
+            dataGridView1.Rows[1].Cells[1].Value = 19;
+            dataGridView1.Rows[2].Cells[1].Value = "МДВ";
+            dataGridView1.Rows[3].Cells[1].Value = 7;
+            dataGridView1.Rows[4].Cells[1].Value = "МТУСИ";
+        }
+
+        private void listBox2_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            if (e.Index > -1)
+            {
+                string itemS = (sender as ListBox).Items[e.Index].ToString();
+              Brush textBrush = Brushes.Black;
+                if ((Convert.ToDouble(itemS) % 1) == 0)
+                {
+                    //textBrush = Brushes.Red;
+                    e.Graphics.DrawRectangle(Pens.Red, e.Bounds);
+                }
+                else
+                {
+                    //textBrush = Brushes.Red;
+                }
+                e.Graphics.DrawString(itemS, e.Font, textBrush, e.Bounds, StringFormat.GenericDefault);
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                text = null;
+                p = Convert.ToInt32(dataGridView1.Rows[0].Cells[1].Value);
+                q = Convert.ToInt32(dataGridView1.Rows[1].Cells[1].Value);
+
+                fi = (p - 1) * (q - 1);
+                n = p * q;
+
+                textBox1.Text = "f(n) = (p - 1) * (q - 1) = " + (p - 1) + " * " + (q - 1) + " = " + fi + "\r\n";
+                textBox1.Text += "n = p * q = " + p + " * " + q + " = " + n + "\r\n";
+
+                listBox1.Items.Clear();
+                bool check;
+                for (int i = 2; i < fi; i++)
+                {
+                    check = true;
+                    for (int k = 2; k <= i; k++) {
+                        if (((fi % k) == 0) && ((i % k)==0))
+                        {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (check)
+                    {
+                        listBox1.Items.Add(i);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                toolStripStatusLabel1.Text = ex.Message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                d = Convert.ToInt32(listBox1.SelectedItem);
+                textBox1.Text += "d = " + d + "\r\n";
+                listBox2.Items.Clear();
+                for (int i = 1; i < fi; i++)
+                {
+                    listBox2.Items.Add((double)((fi * i) + 1) / d);
+                }
+            }
+            catch (Exception ex)
+            {
+                toolStripStatusLabel1.Text = ex.Message;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //RSA
+                string alfavit = "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя";
+                eps = Convert.ToInt32(listBox2.SelectedItem);
+                textBox1.Text += "e = " + eps + "\r\n";
+                text = Convert.ToString(dataGridView1.Rows[2].Cells[1].Value);
+                textBox1.Text += "\r\n" + text + " = (";
+                M = new int[text.Length];
+                for (int i = 0; i < M.Length; i++)
+                {
+                    M[i] = alfavit.IndexOf(text.Substring(i, 1)) / 2 + 1;
+                    textBox1.Text += M[i] + ", ";
+                }
+                textBox1.Text = textBox1.Text.Substring(0, (textBox1.Text.Length - 2)) + ")\r\n";
+
+                C = new int[text.Length];
+                for (int i = 0; i < C.Length; i++)
+                {
+                    C[i] = mod(M[i], eps, n);
+                    textBox1.Text += "C[" + i + "] = (" + M[i] + "^" + eps + ") mod " + n + " = " + C[i] + "\r\n";
+                }
+
+                M2 = new int[text.Length];
+                for (int i = 0; i < M2.Length; i++)
+                {
+                    M2[i] = mod(C[i], d, n);
+                    textBox1.Text += "M2[" + i + "] = (" + C[i] + "^" + d + ") mod " + n + " = " + M2[i] + "\r\n";
+                }
+                //hash
+                text = Convert.ToString(dataGridView1.Rows[4].Cells[1].Value);
+                textBox1.Text += "\r\n" + text + " = (";
+                M = new int[text.Length];
+                for (int i = 0; i < M.Length; i++)
+                {
+                    M[i] = alfavit.IndexOf(text.Substring(i, 1)) / 2 + 1;
+                    textBox1.Text += M[i] + ", ";
+                }
+                textBox1.Text = textBox1.Text.Substring(0, (textBox1.Text.Length - 2)) + ")\r\n";
+
+                H = new int[text.Length + 1];
+                H[0] = (int)dataGridView1.Rows[3].Cells[1].Value;
+                for (int i = 1; i < H.Length; i++)
+                {
+                    H[i] = mod(H[i - 1] + M[i-1], 2, n);
+                    textBox1.Text += "H[" + i + "] = (("+H[i-1] + " + "+ M[i-1] + ")^2) mod " + n + " = " + H[i] + "\r\n";
+                }
+                //подпись
+                S = mod(H[H.Length - 1], d, n);
+                textBox1.Text += "\r\nS = (" + H[H.Length - 1] + "^" + d + ") mod " + n + " = " + S + "\r\n";
+                textBox1.Text += "H = (" + S + "^" + eps + ") mod " + n + " = " + mod(S, eps, n) + "\r\n";
+            }
+            catch (Exception ex)
+            {
+                toolStripStatusLabel1.Text = ex.Message;
+            }
+        }
+
+        private int mod(int delimoe, int stepen, int delitel)
+        {
+            int[] arr = new int[Int16.MaxValue];
+            // = (delimoe^stepen) mod delitel
+            arr[0] = (delimoe % delitel);
+            for (int k = 1; ; k++)
+            {
+                arr[k] = (delimoe * arr[k - 1]) % delitel;
+                if (arr[k] == arr[0])
+                {
+                    return arr[(stepen % k) - 1];
+                }
+            }
+        }
+    }
+}
